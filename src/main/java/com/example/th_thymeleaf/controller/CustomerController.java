9651073cde34947d7c560b079245d5a98cfb04e0
viethungle1 <hunglv0801@gmail.com>
2024-02-28 14:00:28 +0700

    add show list customer

---
+++ src/main/java/com/example/th_thymeleaf/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package com.example.th_thymeleaf.controller;
+
+import com.example.th_thymeleaf.model.Customer;
+import com.example.th_thymeleaf.service.CustomerService;
+import com.example.th_thymeleaf.service.ICustomerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+    private final ICustomerService customerService = new CustomerService();
+
+    @GetMapping("")
+    public String index(Model model) {
+        List<Customer> customerList = customerService.findAll();
+        model.addAttribute("customers", customerList);
+        return "/index";
+    }
+}
---
+++ src/main/java/com/example/th_thymeleaf/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package com.example.th_thymeleaf.service;
+
+import com.example.th_thymeleaf.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
